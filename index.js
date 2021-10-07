9c89af6a8d8012af94a79ab59c2e2556c6aa2935
javaid05 <javaidmalik.khan@gmail.com>
2021-10-07 15:57:16 +0100

    step1

---
+++ index.js
@@ -0,0 +1,8 @@
+
+ let addName = function(){
+  let ul = document.getElementById(dynamicList)
+  let getInputVale = document.getElementById(userName)
+  let li = document.createElement("li")
+  let people =[]
+  people.push(getName)
+ }
