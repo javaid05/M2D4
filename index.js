3ac5762074a6129048b1656b2432660570ddb42f
javaid05 <javaidmalik.khan@gmail.com>
2021-10-08 10:46:02 +0100

    start

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
   let ul = document.getElementById(dynamicList)
   let getInputVale = document.getElementById(userName)
   let li = document.createElement("li")
+  
   let people =[]
   people.push(getName)
  }
